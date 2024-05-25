b39668ce444887230b233b1baa5c6e08087446e2
AMPW-german <theampw@gmail.com>
2024-05-25 14:14:05 +0200

    code update

--- G-Effects/G-Effects.cs
+++ G-Effects/G-Effects.cs
@@ -22,7 +22,7 @@ namespace G_Effects
 	/// <summary>
 	/// The G-Effects plugin for Kerbal Space Program
 	/// 
-	///Copyright (C) 2015-2017 Ser
+	///Copyright (C) 2024 AMPW
 	///This program is free software; you can redistribute it and/or
 	///modify it under the terms of the GNU General Public License
 	///as published by the Free Software Foundation; either version 2
